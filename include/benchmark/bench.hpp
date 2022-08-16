c283102620909e086b3891a082dcddc953be5948
Etienne Palanga <e.palanga@disroot.org>
2022-08-16 18:57:12 +0200

    Fix Segfault when using std::copy

--- include/benchmark/bench.hpp
+++ include/benchmark/bench.hpp
@@ -9,6 +9,7 @@
 #include "../grammar.hpp"
 #include "sampled_scan_query_grammar.hpp"
 #include <malloc_count.h>
+#include <vector>
 
 namespace gracli {
 
@@ -186,11 +187,14 @@ void benchmark_substring(QGrammarResult<std::string> &data,
     std::chrono::steady_clock::time_point begin = std::chrono::steady_clock::now();
     size_t                                c     = 0;
     for (size_t i = 0; i < num_queries; i++) {
-        std::copy(qgr.begin() + (rand() % data.source_length),
-                  qgr.begin() + (rand() % data.source_length + length),
+        size_t start = (rand() % data.source_length);
+        size_t end = std::min(start + length, data.source_length);
+        std::copy(qgr.begin() + start,
+                  qgr.begin() + end,
                   buf);
         c += buf[0];
     }
+
     std::chrono::steady_clock::time_point end = std::chrono::steady_clock::now();
     auto query_time_total = std::chrono::duration_cast<std::chrono::milliseconds>(end - begin).count();
     // so the calls are hopefully not optimized away
