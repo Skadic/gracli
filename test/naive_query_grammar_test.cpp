e937995009958574eda0cf2ca47de59ccf9b5169
Etienne Palanga <e.palanga@disroot.org>
2022-11-14 18:32:35 +0100

    Fix tests

--- test/naive_query_grammar_test.cpp
+++ test/naive_query_grammar_test.cpp
@@ -1,5 +1,5 @@
 #include "query_grammar_tests.hpp"
-#include <naive_query_grammar.hpp>
+#include <grammar/naive_query_grammar.hpp>
 
 class NaiveQGTestFixture : public QueryGrammarTestFixture<gracli::NaiveQueryGrammar> {};
 
@@ -9,5 +9,5 @@ TEST_P(NaiveQGTestFixture, SubstringTest) { test_substr(); }
 
 INSTANTIATE_TEST_SUITE_P(NaiveQGTests,
                          NaiveQGTestFixture,
-                         ::testing::Values(QueryGrammarTestInput("test_data/fox.txt", "test_data/fox.txt.seq", 25)),
-                         QueryGrammarTestInput("test_data/fox.txt", "test_data/fox.txt.rp", 25));
+                         ::testing::Values(QueryGrammarTestInput("test/test_data/fox.txt", "test/test_data/fox.txt.seq", 25)),
+                         QueryGrammarTestInput("test/test_data/fox.txt", "test/test_data/fox.txt.rp", 25));
