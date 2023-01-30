c7b397f5b1f16a79f63d1615814dd84736e6f0e0
Etienne Palanga <e.palanga@disroot.org>
2023-01-30 18:35:39 +0100

    feat!(bt): make blocktrees support the new format

--- include/blocktree/blocktree.hpp
+++ include/blocktree/blocktree.hpp
@@ -7,18 +7,15 @@ namespace gracli {
 
 class BlockTreeRandomAccess {
     std::unique_ptr<CBlockTree> m_cbt;
-    size_t m_source_length;
-    BlockTreeRandomAccess(CBlockTree *bt, size_t source_length) : m_cbt{bt}, m_source_length{source_length} {}
+    BlockTreeRandomAccess(CBlockTree *bt) : m_cbt{bt} {}
 
 public:
     static auto from_file(const std::string &path) -> BlockTreeRandomAccess {
         std::ifstream ifs(path);
 
-        uint64_t len = 0;
-        ifs.read(reinterpret_cast<char*>(&len), sizeof(uint64_t));
         auto *cbt = new CBlockTree(ifs);
         
-        return BlockTreeRandomAccess(cbt, len);
+        return BlockTreeRandomAccess(cbt);
     }
 
     inline auto at(size_t i) -> char {
@@ -26,15 +23,11 @@ public:
     }
 
     inline auto substr(char* buf, size_t i, size_t len) -> char* {
-        // TODO The susbtring method described in the paper is not implemented for block trees
-        for (size_t j = i; j < i + len && j < source_length(); j++) {
-            *(buf++) = at(j);
-        }
-        return buf;
+        return m_cbt->substr(buf, i, len);
     }
 
     inline auto source_length() -> size_t {
-        return this->m_source_length;
+        return this->m_cbt->input_size_;
     }
 };
 
