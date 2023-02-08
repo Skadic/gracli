5855e6af598365390ddef4ab3575f646b3917afb
Etienne Palanga <e.palanga@disroot.org>
2023-02-08 18:03:36 +0100

    refactor!(bench): remove unused information in result lines
    BREAKING CHANGE: only time and space measurements left in result lines are construction time, query time and space usage

--- include/benchmark/bench.hpp
+++ include/benchmark/bench.hpp
@@ -9,54 +9,40 @@
 #include <utility>
 #include <vector>
 
+#include <blocktree/blocktree.hpp>
 #include <concepts.hpp>
 #include <file_access/file_access.hpp>
 #include <grammar/grammar.hpp>
 #include <grammar/sampled_scan_query_grammar.hpp>
 #include <lzend/lzend.hpp>
-#include <blocktree/blocktree.hpp>
 
 #include <compute_lzend.hpp>
 #include <malloc_count.h>
 
 namespace gracli {
 
-template<typename Grm>
-struct QGrammarResult {
-    Grm     gr;
+template<typename DS>
+struct QueryDSResult {
+    DS      ds;
     size_t  source_length;
-    size_t  decode_time;
     size_t  constr_time;
-    int64_t decode_space_delta;
-    int64_t constr_space_delta;
+    int64_t space;
 
-    QGrammarResult(Grm   &&gr,
-                   size_t  source_length,
-                   size_t  decode_time,
-                   size_t  constr_time,
-                   int64_t decode_space_delta,
-                   int64_t constr_space_delta) :
-        gr{std::move(gr)},
+    QueryDSResult(DS &&ds, size_t source_length, size_t constr_time, int64_t space) :
+        ds{std::move(ds)},
         source_length{source_length},
-        decode_time{decode_time},
         constr_time{constr_time},
-        decode_space_delta{decode_space_delta},
-        constr_space_delta{constr_space_delta} {}
+        space{space} {}
 };
 
 template<typename Grm>
-auto build_random_access(const std::string &file) -> QGrammarResult<Grm> {
+auto build_random_access(const std::string &file) -> QueryDSResult<Grm> {
+    using TimePoint = std::chrono::steady_clock::time_point;
 
-    std::chrono::steady_clock::time_point begin       = std::chrono::steady_clock::now();
-    size_t                                space_begin = malloc_count_current();
+    TimePoint begin       = std::chrono::steady_clock::now();
+    size_t    space_begin = malloc_count_current();
 
     Grammar gr = Grammar::from_file(file);
-
-    std::chrono::steady_clock::time_point end       = std::chrono::steady_clock::now();
-    size_t                                space_end = malloc_count_current();
-
-    size_t  decode_time        = std::chrono::duration_cast<std::chrono::milliseconds>(end - begin).count();
-    int64_t decode_space_delta = (int64_t) space_end - (int64_t) space_begin;
 
     begin       = std::chrono::steady_clock::now();
     space_begin = malloc_count_current();
@@ -65,17 +51,20 @@ auto build_random_access(const std::string &file) -> QGrammarResult<Grm> {
 
     auto source_length = qgr.source_length();
 
-    end       = std::chrono::steady_clock::now();
-    space_end = malloc_count_current();
+    TimePoint end       = std::chrono::steady_clock::now();
+    size_t    space_end = malloc_count_current();
 
-    size_t  constr_time        = std::chrono::duration_cast<std::chrono::milliseconds>(end - begin).count();
-    int64_t constr_space_delta = (int64_t) space_end - (int64_t) space_begin;
+    size_t  constr_time = std::chrono::duration_cast<std::chrono::milliseconds>(end - begin).count();
+    int64_t space       = (int64_t) space_end - (int64_t) space_begin;
 
-    return {std::move(qgr), source_length, decode_time, constr_time, decode_space_delta, constr_space_delta};
+    return {std::move(qgr), source_length, constr_time, space};
 }
 
 template<>
-auto build_random_access<std::string>(const std::string &file) -> QGrammarResult<std::string> {
+auto build_random_access<std::string>(const std::string &file) -> QueryDSResult<std::string> {
+    using TimePoint = std::chrono::steady_clock::time_point;
+
+    TimePoint   begin       = std::chrono::steady_clock::now();
     size_t      space_begin = malloc_count_current();
     std::string source;
     {
@@ -84,86 +73,84 @@ auto build_random_access<std::string>(const std::string &file) -> QGrammarResult
         ss << ifs.rdbuf();
         source = ss.str();
     }
-    size_t  space_end          = malloc_count_current();
-    auto    source_length      = source.length();
-    int64_t decode_space_delta = (int64_t) space_end - (int64_t) space_begin;
+    TimePoint end           = std::chrono::steady_clock::now();
+    size_t    space_end     = malloc_count_current();
+    auto      source_length = source.length();
 
-    return {std::move(source), source_length, 0, 0, decode_space_delta, 0};
+    size_t  constr_time = std::chrono::duration_cast<std::chrono::milliseconds>(end - begin).count();
+    int64_t space_delta = (int64_t) space_end - (int64_t) space_begin;
+
+    return {std::move(source), source_length, constr_time, space_delta};
 }
 
 template<>
-auto build_random_access<lz::LzEnd>(const std::string &file) -> QGrammarResult<lz::LzEnd> {
-    using namespace lz;
+auto build_random_access<lz::LzEnd>(const std::string &file) -> QueryDSResult<lz::LzEnd> {
     using TimePoint = std::chrono::steady_clock::time_point;
+    using namespace lz;
 
     // Decoding
-    size_t    space_begin        = malloc_count_current();
-    TimePoint begin              = std::chrono::steady_clock::now();
-    auto [parsing, input_size]   = decode(file);
-    TimePoint end                = std::chrono::steady_clock::now();
-    size_t    space_end          = malloc_count_current();
-    size_t    decode_time        = std::chrono::duration_cast<std::chrono::milliseconds>(end - begin).count();
-    int64_t   decode_space_delta = (int64_t) space_end - (int64_t) space_begin;
+    size_t    space_begin      = malloc_count_current();
+    TimePoint begin            = std::chrono::steady_clock::now();
+    auto [parsing, input_size] = decode(file);
 
     // Construct DS
-    space_begin                = malloc_count_current();
-    begin                      = std::chrono::steady_clock::now();
-    lz::LzEnd lz_end           = lz::LzEnd::from_parsing(std::move(parsing), input_size);
-    space_end                  = malloc_count_current();
-    end                        = std::chrono::steady_clock::now();
-    size_t  constr_time        = std::chrono::duration_cast<std::chrono::milliseconds>(end - begin).count();
-    int64_t constr_space_delta = (int64_t) space_end - (int64_t) space_begin;
+    lz::LzEnd lz_end = lz::LzEnd::from_parsing(std::move(parsing), input_size);
+    TimePoint end    = std::chrono::steady_clock::now();
+
+    size_t  space_end   = malloc_count_current();
+    size_t  constr_time = std::chrono::duration_cast<std::chrono::milliseconds>(end - begin).count();
+    int64_t space       = (int64_t) space_end - (int64_t) space_begin;
 
     size_t source_length = lz_end.source_length();
-    return {std::move(lz_end), source_length, decode_time, constr_time, decode_space_delta, constr_space_delta};
+    return {std::move(lz_end), source_length, constr_time, space};
 }
 
 template<>
-auto build_random_access<FileAccess>(const std::string &file) -> QGrammarResult<FileAccess> {
+auto build_random_access<FileAccess>(const std::string &file) -> QueryDSResult<FileAccess> {
     using namespace lz;
     using TimePoint = std::chrono::steady_clock::time_point;
 
     // Decoding
-    size_t    space_begin        = malloc_count_current();
-    TimePoint begin              = std::chrono::steady_clock::now();
-    auto      file_access        = FileAccess::from_file(file);
-    TimePoint end                = std::chrono::steady_clock::now();
-    size_t    space_end          = malloc_count_current();
-    size_t    decode_time        = std::chrono::duration_cast<std::chrono::milliseconds>(end - begin).count();
-    int64_t   decode_space_delta = (int64_t) space_end - (int64_t) space_begin;
+    size_t    space_begin = malloc_count_current();
+    TimePoint begin       = std::chrono::steady_clock::now();
+    auto      file_access = FileAccess::from_file(file);
+    TimePoint end         = std::chrono::steady_clock::now();
+    size_t    space_end   = malloc_count_current();
+    size_t    time        = std::chrono::duration_cast<std::chrono::milliseconds>(end - begin).count();
+    int64_t   space_delta = (int64_t) space_end - (int64_t) space_begin;
 
     const size_t source_length = file_access.source_length();
-    return {std::move(file_access), source_length, decode_time, 0, decode_space_delta, 0};
+    return {std::move(file_access), source_length, time, space_delta};
 }
 
 template<>
-auto build_random_access<BlockTreeRandomAccess>(const std::string &file) -> QGrammarResult<BlockTreeRandomAccess> {
+auto build_random_access<BlockTreeRandomAccess>(const std::string &file) -> QueryDSResult<BlockTreeRandomAccess> {
     using namespace lz;
     using TimePoint = std::chrono::steady_clock::time_point;
 
     // Decoding
-    size_t    space_begin        = malloc_count_current();
-    TimePoint begin              = std::chrono::steady_clock::now();
-    auto      bt        = BlockTreeRandomAccess::from_file(file);
-    TimePoint end                = std::chrono::steady_clock::now();
-    size_t    space_end          = malloc_count_current();
-    size_t    decode_time        = std::chrono::duration_cast<std::chrono::milliseconds>(end - begin).count();
-    int64_t   decode_space_delta = (int64_t) space_end - (int64_t) space_begin;
+    size_t    space_begin = malloc_count_current();
+    TimePoint begin       = std::chrono::steady_clock::now();
+    auto      bt          = BlockTreeRandomAccess::from_file(file);
+    TimePoint end         = std::chrono::steady_clock::now();
+    size_t    space_end   = malloc_count_current();
+    size_t    time        = std::chrono::duration_cast<std::chrono::milliseconds>(end - begin).count();
+    int64_t   space_delta = (int64_t) space_end - (int64_t) space_begin;
 
     const size_t source_length = bt.source_length();
-    return {std::move(bt), source_length, decode_time, 0, decode_space_delta, 0};
+    return {std::move(bt), source_length, time, space_delta};
 }
 
 template<CharRandomAccess Grm>
-void benchmark_random_access(QGrammarResult<Grm> &&data,
-                             const std::string    &file,
-                             size_t                num_queries,
-                             const std::string    &name) {
+void benchmark_random_access(QueryDSResult<Grm> &&data,
+                             const std::string   &file,
+                             size_t               num_queries,
+                             const std::string   &name) {
     std::random_device                    rd;
     std::mt19937                          gen(rd());
     std::uniform_int_distribution<size_t> rand_int(0, data.source_length - 1);
 
-    Grm &qgr = data.gr;
+    Grm &qgr = data.ds;
 
     size_t c = 0;
 
@@ -190,30 +177,28 @@ void benchmark_random_access(QGrammarResult<Grm> &&data,
     std::cout << "RESULT"
               << " type=random_access"
               << " ds=" << name << " input_file=" << file_name << " input_size=" << data.source_length
-              << " num_queries=" << num_queries << " construction_time=" << data.constr_time
-              << " decode_space_delta=" << data.decode_space_delta
-              << " construction_space_delta=" << data.constr_space_delta << " decode_time=" << data.decode_time
+              << " num_queries=" << num_queries << " space=" << data.space << " construction_time=" << data.constr_time
               << " query_time_total=" << query_time_total << std::endl;
 }
 
 template<CharRandomAccess Grm>
 void benchmark_random_access(const std::string &file, size_t num_queries, const std::string &name) {
-    QGrammarResult<Grm> result = build_random_access<Grm>(file);
+    QueryDSResult<Grm> result = build_random_access<Grm>(file);
 
     benchmark_random_access<Grm>(std::move(result), file, num_queries, name);
 }
 
 template<Substring Grm>
-void benchmark_substring(QGrammarResult<Grm> &&data,
-                         const std::string    &file,
-                         size_t                num_queries,
-                         size_t                length,
-                         const std::string    &name) {
+void benchmark_substring(QueryDSResult<Grm> &&data,
+                         const std::string   &file,
+                         size_t               num_queries,
+                         size_t               length,
+                         const std::string   &name) {
     std::random_device                    rd;
     std::mt19937                          gen(rd());
     std::uniform_int_distribution<size_t> rand_int(0, data.source_length - 1);
 
-    Grm &qgr = data.gr;
+    Grm &qgr = data.ds;
 
     char buf[length];
 
@@ -241,23 +226,20 @@ void benchmark_substring(QGrammarResult<Grm> &&data,
     std::cout << "RESULT"
               << " type=substring"
               << " ds=" << name << " input_file=" << file_name << " input_size=" << data.source_length
-              << " num_queries=" << num_queries << " substring_length=" << length
-              << " construction_time=" << data.constr_time << " decode_time=" << data.decode_time
-              << " decode_space_delta=" << data.decode_space_delta
-              << " construction_space_delta=" << data.constr_space_delta << " query_time_total=" << query_time_total
-              << std::endl;
+              << " num_queries=" << num_queries << " substring_length=" << length << " space=" << data.space
+              << " construction_time=" << data.constr_time << " query_time_total=" << query_time_total << std::endl;
 }
 
-void benchmark_substring(QGrammarResult<std::string> &&data,
-                         const std::string            &file,
-                         size_t                        num_queries,
-                         size_t                        length,
-                         const std::string            &name) {
+void benchmark_substring(QueryDSResult<std::string> &&data,
+                         const std::string           &file,
+                         size_t                       num_queries,
+                         size_t                       length,
+                         const std::string           &name) {
     std::random_device                    rd;
     std::mt19937                          gen(rd());
     std::uniform_int_distribution<size_t> rand_int(0, data.source_length - 1);
 
-    std::string &qgr = data.gr;
+    std::string &qgr = data.ds;
 
     char buf[length];
 
@@ -288,21 +270,18 @@ void benchmark_substring(QGrammarResult<std::string> &&data,
     std::cout << "RESULT"
               << " type=substring"
               << " ds=" << name << " input_file=" << file_name << " input_size=" << data.source_length
-              << " num_queries=" << num_queries << " substring_length=" << length
-              << " construction_time=" << data.constr_time << " decode_time=" << data.decode_time
-              << " decode_space_delta=" << data.decode_space_delta
-              << " construction_space_delta=" << data.constr_space_delta << " query_time_total=" << query_time_total
-              << std::endl;
+              << " num_queries=" << num_queries << " substring_length=" << length << " space=" << data.space
+              << " construction_time=" << data.constr_time << " query_time_total=" << query_time_total << std::endl;
 }
 
 template<Substring Grm>
 void benchmark_substring(std::string file, size_t num_queries, size_t length, std::string name) {
-    QGrammarResult<Grm> result = build_random_access<Grm>(file);
+    QueryDSResult<Grm> result = build_random_access<Grm>(file);
     benchmark_substring<Grm>(std::move(result), file, num_queries, length, name);
 }
 
 void benchmark_substring(std::string file, size_t num_queries, size_t length, const std::string &name) {
-    QGrammarResult<std::string> result = build_random_access<std::string>(file);
+    QueryDSResult<std::string> result = build_random_access<std::string>(file);
     benchmark_substring(std::move(result), file, num_queries, length, name);
 }
 
